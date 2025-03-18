597b2d37c1d42be0e734e2dc3586891f8c434416
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-18 12:03:13 +0000

    Feat: adding common attributes of all games

--- src/main/java/com/aamir/abstractGame.java
+++ src/main/java/com/aamir/abstractGame.java
@@ -1,5 +1,28 @@
 package com.aamir;
 
-public class abstractGame {
+
+//Common attributes and methods for all games in library
+public abstract class abstractGame implements playable {
+    protected String title;
+    protected String platform;
+    protected String developer;
+    protected String genre;
+    protected int releaseYear;
+
+    //Constructor
+    public abstractGame(String title, String genre, String platform, int releaseYear, String developer) {
+        this.title = title;
+        this.genre = genre;
+        this.platform = platform;
+        this.releaseYear = releaseYear;
+        this.developer = developer;
+    }
+
+
+    public String getTitle(){
+        return title;
+    }
+
     
 }
+
