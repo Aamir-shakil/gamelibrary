680ad077b9f626c3c108f7b06a1f5f9996d8a6dd
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-18 13:26:18 +0000

    Testing multiplayer class

--- src/main/java/com/aamir/testGame.java
+++ src/main/java/com/aamir/testGame.java
@@ -3,8 +3,9 @@ package com.aamir;
 public class testGame {
     public static void main(String[] args) {
         singlePlayer game1 = new singlePlayer("The Last of Us", "Action-Adventure", "PlayStation", 2013, "Naughty Dog");
-
+        multiplayer game2 = new multiplayer("Call of Duty: Warzone", "Shooter", "PlayStation & Xbox", 2020, "Infinity Ward");
         game1.updateProgress();
+        game2.updateProgress();
     }
     
 }
