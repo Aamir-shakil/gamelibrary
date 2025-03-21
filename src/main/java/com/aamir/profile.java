775f55a9806369df252ae907678b671d0f97ff0a
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-21 12:06:34 +0000

    Feat: Class for individual profile

---
+++ src/main/java/com/aamir/profile.java
@@ -0,0 +1,13 @@
+package com.aamir;
+
+public class profile {
+    private String username;
+    private String userPlatform;
+
+
+    public profile(String username, String userPlatform){
+        this.username = username;
+        this.userPlatform = userPlatform;
+    }
+    
+}
