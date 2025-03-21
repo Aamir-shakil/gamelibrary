5d31be6e1b2d8f81ff22c3d72b0aa869d4b6a6fc
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-21 12:32:03 +0000

    Feat: Test to check search function

--- src/main/java/com/aamir/testGame.java
+++ src/main/java/com/aamir/testGame.java
@@ -1,11 +1,25 @@
 package com.aamir;
+
+import java.util.List;
+
 //Testing to see if the singlePlayer class works
 public class testGame {
     public static void main(String[] args) {
-        singlePlayer game1 = new singlePlayer("The Last of Us", "Action-Adventure", "PlayStation", 2013, "Naughty Dog");
-        multiplayer game2 = new multiplayer("Call of Duty: Warzone", "Shooter", "PlayStation & Xbox", 2020, "Infinity Ward");
-        game1.updateProgress();
-        game2.updateProgress();
+        // singlePlayer game1 = new singlePlayer("The Last of Us", "Action-Adventure", "PlayStation", 2013, "Naughty Dog");
+        // multiplayer game2 = new multiplayer("Call of Duty: Warzone", "Shooter", "PlayStation & Xbox", 2020, "Infinity Ward");
+        // game1.updateProgress();
+        // game2.updateProgress();
+        profile aamirShakil = new profile("Aamir", "PC");
+        aamirShakil.addGame(new singlePlayer("Elden Ring", "RPG", "PS5", 2022, "FromSoftware"));
+         List<abstractGame> searchResults = aamirShakil.searchGame("Elden");
+
+        // Output the search results
+        System.out.println("Search Results:");
+        for (abstractGame game : searchResults) {
+            System.out.println("Found " + game.getTitle()); 
+        }
+
+
     }
     
 }
