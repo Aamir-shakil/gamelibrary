4672cfac9581017b75540c81d2fb21e13150deab
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-21 12:11:42 +0000

    Update: Function to add game in profile class

--- src/main/java/com/aamir/profile.java
+++ src/main/java/com/aamir/profile.java
@@ -1,13 +1,23 @@
 package com.aamir;
+import java.util.List;
+import java.util.ArrayList;
 
 public class profile {
     private String username;
     private String userPlatform;
+    private List<abstractGame> games;
 
 
     public profile(String username, String userPlatform){
         this.username = username;
         this.userPlatform = userPlatform;
+        this.games = new ArrayList<abstractGame>();
+    }
+
+
+
+    public void addGame(abstractGame game){
+        games.add(game);
     }
     
 }
