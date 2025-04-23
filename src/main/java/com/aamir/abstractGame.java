791f3c0f520f9443c8a60b4bcf2c413bd8b36869
Aamir-shakil <aamirshakil21@gmail.com>
2025-04-23 13:34:12 +0100

    Update: abstractGame Class

--- src/main/java/com/aamir/abstractGame.java
+++ src/main/java/com/aamir/abstractGame.java
@@ -1,61 +1,41 @@
 package com.aamir;
 
 import com.aamir.models.playable;
-import javafx.beans.property.*;
 
+// Common attributes and methods for all games in library
 public abstract class abstractGame implements playable {
-    protected StringProperty title;
-    protected StringProperty platform;
-    protected StringProperty developer;
-    protected StringProperty genre;
-    protected IntegerProperty releaseYear;
+    protected String title;
+    protected String platform;
+    protected String developer;
+    protected String genre;
+    protected int releaseYear;
 
+    // Constructor
     public abstractGame(String title, String genre, String platform, int releaseYear, String developer) {
-        this.title = new SimpleStringProperty(title);
-        this.genre = new SimpleStringProperty(genre);
-        this.platform = new SimpleStringProperty(platform);
-        this.releaseYear = new SimpleIntegerProperty(releaseYear);
-        this.developer = new SimpleStringProperty(developer);
+        this.title = title;
+        this.genre = genre;
+        this.platform = platform;
+        this.releaseYear = releaseYear;
+        this.developer = developer;
     }
 
-    // Getters for TableView
     public String getTitle() {
-        return title.get();
-    }
-
-    public StringProperty titleProperty() {
         return title;
     }
 
-    public String getPlatform() {
-        return platform.get();
+    public String getGenre() {
+        return genre;
     }
 
-    public StringProperty platformProperty() {
+    public String getPlatform() {
         return platform;
     }
 
     public String getDeveloper() {
-        return developer.get();
-    }
-
-    public StringProperty developerProperty() {
         return developer;
     }
 
-    public String getGenre() {
-        return genre.get();
-    }
-
-    public StringProperty genreProperty() {
-        return genre;
-    }
-
     public int getReleaseYear() {
-        return releaseYear.get();
-    }
-
-    public IntegerProperty releaseYearProperty() {
         return releaseYear;
     }
 
